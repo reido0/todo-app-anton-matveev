bb2d3ad7c7da5f8e006c8e3bb9ad4de3e952e38a
Matveev Anton <reido@mail.ru>
2021-11-10 17:33:47 +0300

    feat: add render cards owner

--- src/components/cards/cards.component.jsx
+++ src/components/cards/cards.component.jsx
@@ -30,39 +30,41 @@ const Cards = ({
 
     return (
         <StyledContainer>
-            {MOCKED_CARDS.map((card) =>
-                <StyledCard
-                    key={card}
-                >
-                    <div className={"card-item"}>
-                        <button className={"btn-card-edit"}>
-                            <i className={"fas fa-pencil-alt"}></i>
-                        </button>
-                        <p className={"card-title-task"}>Task:</p>
-                        <div className={"card-taskname-wrapper"}>
-                            <p className={"card-taskname"}>{`${card.taskName}`}</p>
-                            <p className={"card-taskname-status"}>{`${card.isDone ? 'done' : 'not done'}`}</p>
-                        </div>
-                        <p className={"card-title-discription"}>Discription:</p>
-                        <p className={"card-discription"}>{`${card.taskDescription}`}</p>
-                        <p className={"finish-date"}>Date: {new Date().toDateString()}</p>
-                        <div className={"avatar"}>
-                            <p className={"card-title-username"}>User name:</p>
-                            <p className={"card-username"}>{`${card.userName}`}</p>
-                            <div className={"avatar-img"}>
+            {MOCKED_CARDS.map((card) => (
+                <React.Fragment key={card.taskName}>
+                    {card.owner === userId && (
+                        <StyledCard>
+                            <div className={"card-item"}>
+                                <button className={"btn-card-edit"}>
+                                    <i className={"fas fa-pencil-alt"}></i>
+                                </button>
+                                <p className={"card-title-task"}>Task:</p>
+                                <div className={"card-taskname-wrapper"}>
+                                    <p className={"card-taskname"}>{`${card.taskName}`}</p>
+                                    <p className={"card-taskname-status"}>{`${card.isDone ? 'done' : 'not done'}`}</p>
+                                </div>
+                                <p className={"card-title-discription"}>Discription:</p>
+                                <p className={"card-discription"}>{`${card.taskDescription}`}</p>
+                                <p className={"finish-date"}>Date: {new Date().toDateString()}</p>
+                                <div className={"avatar"}>
+                                    <p className={"card-title-username"}>User name:</p>
+                                    <p className={"card-username"}>{`${card.userName}`}</p>
+                                    <div className={"avatar-img"}>
+                                    </div>
+                                </div>
+                                <div className={"card-btn-wrapper"}>
+                                    <button className={"card-btn"}>MoveUp</button>
+                                    <button className={"card-btn"}>MoveDown</button>
+                                    <button className={"card-btn"}>To Do</button>
+                                    <button className={"card-btn"}>In Progress</button>
+                                    <button className={"card-btn"}>Done</button>
+                                    <button className={"card-btn"}>Delete</button>
+                                </div>
                             </div>
-                        </div>
-                        <div className={"card-btn-wrapper"}>
-                            <button className={"card-btn"}>MoveUp</button>
-                            <button className={"card-btn"}>MoveDown</button>
-                            <button className={"card-btn"}>To Do</button>
-                            <button className={"card-btn"}>In Progress</button>
-                            <button className={"card-btn"}>Done</button>
-                            <button className={"card-btn"}>Delete</button>
-                        </div>
-                    </div>
-                </StyledCard>
-            )}
+                        </StyledCard>
+                    )}
+                </React.Fragment>
+            ))}
         </StyledContainer >
     );
 };
